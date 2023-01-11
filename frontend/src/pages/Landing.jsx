47fc24a5a2ff5685c68efff6251e0a7ef740d82f
Johanna <johanna.stoicanescu@gmail.com>
2023-01-11 17:36:01 +0100

    changes

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,5 +1,30 @@
+import { useNavigate } from "react";
+import "./Landing.css";
+import NavBar from "../components/NavBar";
+
 function Landing() {
-  return <div>Landing</div>;
+  const navigate = useNavigate();
+
+  const handleToLookingFor = () => {
+    navigate("/looking-for");
+  };
+
+  const handleToRent = () => {
+    navigate("/rent");
+  };
+
+  return (
+    <div className="landing-page">
+      <NavBar />
+      <img src="../assets/logo.png" alt="" />
+      <button type="button" onClick={handleToLookingFor()}>
+        Pick ma ride
+      </button>
+      <button type="button" onClick={handleToRent()}>
+        Rent a ride
+      </button>
+    </div>
+  );
 }
 
 export default Landing;
