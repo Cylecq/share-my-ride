2768cdc0cf2af7cafbade10b15cc4f92bba1f306
Johanna <johanna.stoicanescu@gmail.com>
2023-01-11 19:18:00 +0100

    changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,6 @@ import SignUp from "./pages/SignUp";
 import LogIn from "./pages/LogIn";
 import UsersRides from "./pages/UsersRides";
 import LookingFor from "./pages/Rent/LookingFor";
-import Details from "./pages/Rent/Details";
 import ChooseRide from "./pages/Rent/ChooseRide";
 import Rent from "./pages/RentTo/Rent";
 import Modify from "./pages/RentTo/Modify";
@@ -20,11 +19,11 @@ function App() {
           <Route path="/signup" element={<SignUp />} />
           <Route path="/login" element={<LogIn />} />
           <Route path="/look" element={<LookingFor />} />
-          <Route path="/details" element={<Details />} />
           <Route path="/choose" element={<ChooseRide />} />
           <Route path="/rent" element={<Rent />} />
-          <Route path="/history" element={<UsersRides />} />
+          <Route path="/myrides" element={<UsersRides />} />
           <Route path="/modify" element={<Modify />} />
+          <Route path="/myreservasion" element={<UsersRides />} />
         </Routes>
       </Router>
     </div>
