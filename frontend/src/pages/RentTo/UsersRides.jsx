21b66046b7b7e01c1431072f45c0ca9cd0544dd7
Johanna <johanna.stoicanescu@gmail.com>
2023-01-12 19:45:26 +0100

    all ma rides page

--- frontend/src/pages/RentTo/UsersRides.jsx
+++ frontend/src/pages/RentTo/UsersRides.jsx
@@ -1,6 +1,6 @@
 import useFetch from "../../services/useFetch";
 import NavBar from "../../components/NavBar";
-import Footer from "../../components/Footer";
+// import Footer from "../../components/Footer";
 import RidesCard from "../components/RidesCard";
 
 function UsersRides() {
@@ -12,7 +12,7 @@ function UsersRides() {
     <div className="myreservation-page">
       <NavBar />
       <div className="main-myreservation">
-        <h3 className="title-page">My reservation</h3>
+        <h3 className="title-page">All ma rides</h3>
         <div className="vehicle-list">
           {loading && <div>LOADING...</div>}
           {vehicles &&
@@ -22,11 +22,12 @@ function UsersRides() {
                 id={vehicle.id}
                 name={vehicle.name}
                 price={vehicle.price}
+                photo={vehicle.photo}
               />
             ))}
         </div>
       </div>
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
