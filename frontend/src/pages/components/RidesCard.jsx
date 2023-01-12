40fdcf0e89e2b0cdc86a02073c3438dbd29ed316
Cylecq <leclercq.cyril06@gmail.com>
2023-01-12 18:18:23 +0100

    ChooseRide link done + LookingFor fetch done + RidesCard HTML done

--- frontend/src/pages/components/RidesCard.jsx
+++ frontend/src/pages/components/RidesCard.jsx
@@ -1,5 +1,17 @@
-function RidesCard() {
-  return <div>RidesCard</div>;
+import "./RidesCard.css";
+
+const URL_BACKEND = import.meta.env.VITE_BACKEND_URL;
+
+function RidesCard({ name, photo, price }) {
+  return (
+    <div className="card">
+      <div className="card-image">
+        <img src={`${URL_BACKEND}/vehiclePicture/${photo}`} alt="{name}" />
+        <div className="card-title">{name}</div>
+        <div className="card-price">{price}€/day</div>
+      </div>
+    </div>
+  );
 }
 
 export default RidesCard;
