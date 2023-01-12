6902be85df9b0055fc4a688a507615f88495b0d3
Johanna <johanna.stoicanescu@gmail.com>
2023-01-12 17:00:51 +0100

    footer and modif css navbar

--- frontend/src/pages/Form/LoginForm.jsx
+++ frontend/src/pages/Form/LoginForm.jsx
@@ -1,27 +1,31 @@
-import React from "react";
+import Footer from "../../components/Footer";
 import "./LoginForm.css";
 // import "../Landing.css";
 
 function LoginForm() {
   return (
-    <div>
-      <h1>Log In</h1>
-      <form className="loginform">
-        <label>
-          Ma mail
-          <input type="text" name="mail" />
-        </label>
+    <div className="login-page">
+      <div>
+        <h1>Log In</h1>
+        <form className="loginform">
+          <label>
+            Ma mail
+            <input type="text" name="mail" />
+          </label>
 
-        <label>
-          Ma password
-          <input type="text" name="password" />
-        </label>
-        <div className="buttonbox">
-          <button type="button" className="buttons" id="loginbutton">
-            Submit
-          </button>
-        </div>
-      </form>
+          <label>
+            Ma password
+            <input type="text" name="password" />
+          </label>
+          <div className="buttonbox">
+            <button type="button" className="buttons" id="loginbutton">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+
+      <Footer />
     </div>
   );
 }
